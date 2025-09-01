c11911229671521389d41aba7b73941441d58bec
jeremycodes-tech <jeremybraka56@gmail.com>
2025-09-01 16:55:40 +0300

    Removed assets.ts and updated image imports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",   // ✅ this replaces "next export"
   images: {
     unoptimized: true,
   },
