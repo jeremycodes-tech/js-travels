f2999e33797701eca27765bf037ffb3da1babffa
jeremycodes-tech <jeremybraka56@gmail.com>
2025-09-01 17:17:37 +0300

    Removed assets.ts and updated image imports

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
+const repoName = "js-travels"; // must match repo name exactly
+const isProd = process.env.NODE_ENV === "production";
+const isVercel = !!process.env.VERCEL;
+
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",   // ✅ this replaces "next export"
+  output: "export",
   images: {
     unoptimized: true,
+  },
+  // GitHub Pages needs basePath & assetPrefix
+  assetPrefix: !isVercel && isProd ? `/${repoName}/` : "",
+  basePath: !isVercel && isProd ? `/${repoName}` : "",
+  env: {
+    NEXT_PUBLIC_BASE_PATH: !isVercel && isProd ? `/${repoName}` : "",
   },
 };
 
 module.exports = nextConfig;
+
+// Force GH Pages locally
+if (process.env.TEST_GHPAGES) {
+  module.exports.basePath = "/js-travels";
+  module.exports.assetPrefix = "/js-travels/";
+  module.exports.env.NEXT_PUBLIC_BASE_PATH = "/js-travels";
+}
