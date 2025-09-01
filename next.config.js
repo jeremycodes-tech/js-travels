7143d1a0c469a303506ea6e62be3a95216e319e6
jeremycodes-tech <jeremybraka56@gmail.com>
2025-09-01 16:48:01 +0300

    Removed assets.ts and updated image imports

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
-const repoName = "js-travels"; // must match GitHub repo name exactly
-const isProd = process.env.NODE_ENV === "production";
-const isVercel = !!process.env.VERCEL; // detects if running on Vercel
-
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   images: {
     unoptimized: true,
-  },
-  assetPrefix: !isVercel && isProd ? `/${repoName}/` : "",
-  basePath: !isVercel && isProd ? `/${repoName}` : "",
-  env: {
-    NEXT_PUBLIC_BASE_PATH: !isVercel && isProd ? `/${repoName}` : "",
   },
 };
 
