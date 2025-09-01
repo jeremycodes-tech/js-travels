3bb3f1f4415e54f26e470b3e2f02615720c60b74
jeremycodes-tech <jeremybraka56@gmail.com>
2025-09-01 17:06:15 +0300

    Removed assets.ts and updated image imports

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image 
-          src={("/HIlink-logo.svg")} 
+          src={("/hilink-logo.svg")} 
           alt="logo" 
           width={74} 
           height={29} 
